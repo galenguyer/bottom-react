0137dfb16c1b7f3125719441765fce413a1c4eaf
Galen Guyer <galen@galenguyer.com>
2021-02-17 21:44:54 -0500

    translate either way

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import {encode, decode} from 'bottomify';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      { encode('Hello, World!') }
+      <h1>Bottom Text</h1>
+      <textarea id='top-text' spellCheck='false' autoComplete='false' placeholder='top text' onInput={() => translateToBottom()}></textarea>
+      <br />
+      <br />
+      <textarea id='bottom-text' spellCheck='false' autoComplete='false' placeholder='bottom text' onInput={() => translateToTop()}></textarea>
     </div>
   );
 }
+
+function translateToBottom() {
+  if (document.getElementById('bottom-text') == null || document.getElementById('top-text') == null) {
+    return; 
+  }
+  document.getElementById('bottom-text').value = encode(document.getElementById('top-text').value);
+}
+
+function discordToEmoji(text) {
+  
+}
+
+function translateToTop() {
+  if (document.getElementById('bottom-text') == null || document.getElementById('top-text') == null) {
+    return; 
+  }
+  document.getElementById('top-text').value = decode(document.getElementById('bottom-text').value);
+}
+
 
 export default App;
